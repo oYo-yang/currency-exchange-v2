2bb777fb5b3e03e402a279fcfbeecfac5a3c0037
Trista <trista_23@163.com>
2025-07-25 14:39:12 +0800

    add currency frontend

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
+app.use(express.static('public'))
 app.use('/currency', currencyRouter)
 app.use('/user', userRouter)
 app.use('/watchlist', watchlistRouter)
